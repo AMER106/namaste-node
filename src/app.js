9e05a74c9842281a665cfbfacd230f042806d0b2
AMER106 <arabamer031@gmail.com>
2025-12-09 23:22:05 +0530

    exploring http methods

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 import express from "express";
 const app = express();
+
+app.use("/about", (req, res) => {
+  res.send("hello this is our about page");
+});
+app.get("/user", (req, res) => {
+  res.send("hello user");
+});
+app.post("/user", (req, res) => {
+  res.send("hey user you are saved in our db");
+});
 app.use("/", (req, res) => {
   res.send("Helo from express server");
 });
-app.listen(3000, () => {
-  console.log("server is runnning on port 3000");
+app.listen(3001, () => {
+  console.log("server is runnning on port 3001");
 });
